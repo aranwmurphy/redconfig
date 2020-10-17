cfab6c8a9861a5b567d10d12441da34f34e1605d
Aran Murphy <aranwmurphy@gmail.com>
2020-10-17 18:58:05 -0400

    Built library

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,13 @@
 import { Redis } from "ioredis";
 export interface IRedConfig {
+}
+export interface IEnvRedConfig {
     [key: string]: string;
 }
-export declare class RedConfig {
+export declare class RedConfig<TConfig extends IRedConfig = IEnvRedConfig> {
     readonly client: Redis;
     readonly name: string;
     constructor(client: Redis, name: string);
-    save(config: IRedConfig): Promise<IRedConfig>;
-    load(): Promise<IRedConfig>;
+    save(config: TConfig): Promise<TConfig>;
+    load(): Promise<TConfig>;
 }
